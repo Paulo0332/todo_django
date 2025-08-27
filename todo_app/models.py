fe9a065ba1600bdc091b0032c849ef3b5d27bd9d
Paulo <paulovitor.lavinas@gmail.com>
2025-08-27 18:57:30 -0300

    chore(admin): register Task and add __str__

--- todo_app/models.py
+++ todo_app/models.py
@@ -8,3 +8,6 @@ class Task(models.Model):
     description = models.CharField(max_length=255,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
