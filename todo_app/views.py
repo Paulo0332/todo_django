e4ec49afde67a6c711830d0955336b847b904fd9
Paulo <paulovitor.lavinas@gmail.com>
2025-08-28 11:39:48 -0300

    feat: creating views

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views.generic import ListView,UpdateView,DeleteView,CreateView
+from django.urls import reverse_lazy
+from .models import Task
 
 # Create your views here.
+
+class TaskViewView(ListView):
+    model = Task
+    template_name = "tasks/task_list.html"
+    context_object_name = "tasks"
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = "tasks/task_form.html"
+    fields = ["name","flag","description"]
+    success_url = reverse_lazy('task-list')
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "tasks/task_form.html"
+    fields = ["name", "flag","description"]
+    success_url = reverse_lazy('task-list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "task/task_confirm_delete.html"
+    success_url = reverse_lazy('task-list')
